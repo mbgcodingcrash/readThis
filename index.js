4717a284c7926a1f6aa262e53c342e7d63b6b44f
mbgcodingcrash <re4u2c@gmail.com>
2022-06-19 21:36:33 -0400

    updated to older version of inquirer since thats the version that the class taught us

--- index.js
+++ index.js
@@ -1,10 +1,42 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquirer')
-const fs = require('fs');
+const inquirer = require ('inquirer')
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project'
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Provide installation instructions for your project'
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide usage information for your project'
+      },
+      {
+        type: 'input',
+        name: 'contributions',
+        message: 'Provide contribution guidelines for your project'
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Provide test instructions for your project'
+      }
+    ]);
+  };
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
